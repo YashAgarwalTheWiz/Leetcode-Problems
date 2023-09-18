b8c9ce050b75b5a534318298806d1b331327936e
YashAgarwalTheWiz <143926886+YashAgarwalTheWiz@users.noreply.github.com>
2023-09-18 10:23:18 +0530

    Time: 0 ms (100%), Space: 6.2 MB (12.95%) - LeetHub

--- 0029-divide-two-integers/0029-divide-two-integers.cpp
+++ 0029-divide-two-integers/0029-divide-two-integers.cpp
@@ -1,20 +1,8 @@
 class Solution {
 public:
-    int divide(int dividend, int divisor) {
-        if(dividend == 0) {
-            return 0;
-        }
-        if(dividend == -2147483648 && divisor == -1) {
-            return 2147483647;
-        }
-        if(dividend == 1000000000 && divisor == 1) {
-            return 1000000000;
-        }
-        int sign = ((dividend) < 0) ^ (divisor < 0)? -1: 1;
-        long long int ans = 0;
-        long long int dd = abs(dividend);
-        long long int dv = abs(divisor);
-        ans = exp((log(dd)) - (log(dv))) + 0.0000000001;
-        return sign*ans; 
+    long long divide(long long m, long long n) {
+        if (m == -2147483648 && n == -1) return (2147483647);
+        long long q =m/n;
+      return q;
     }
 };
