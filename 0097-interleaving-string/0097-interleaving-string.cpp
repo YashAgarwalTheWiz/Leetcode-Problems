9d8811465ec32c4c9c46821ed530d560487a972e
YashAgarwalTheWiz <143926886+YashAgarwalTheWiz@users.noreply.github.com>
2023-10-26 15:37:49 +0530

    Time: 27 ms (20.47%), Space: 34.5 MB (15.44%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    bool solve(string s1, string s2, string s3, int i, int j, int k, vector<vector<vector<int>>>& memo) {
-        if (i == s1.size() && j == s2.size() && k == s3.size()) {
+    bool solve(string s1, string s2, string s3, int i, int j, vector<vector<int>>& memo) {
+        if (i == s1.size() && j == s2.size()) {
             return true;
         }
 
-        if (memo[i][j][k] != -1) {
-            return memo[i][j][k];
+        if (memo[i][j] != -1) {
+            return memo[i][j];
         }
 
         bool x = false, y = false;
 
-        if (i != s1.size() && s1[i] == s3[k]) {
-            x = solve(s1, s2, s3, i + 1, j, k + 1, memo);
+        if (i != s1.size() && s1[i] == s3[i + j]) {
+            x = solve(s1, s2, s3, i + 1, j, memo);
         }
 
-        if (j != s2.size() && s2[j] == s3[k]) {
-            y = solve(s1, s2, s3, i, j + 1, k + 1, memo);
+        if (j != s2.size() && s2[j] == s3[i + j]) {
+            y = solve(s1, s2, s3, i, j + 1, memo);
         }
 
-        return memo[i][j][k] = (x || y);
+        return memo[i][j] = (x || y);
     }
 
     bool isInterleave(string s1, string s2, string s3) {
@@ -31,9 +31,8 @@ public:
             return false;
         }
 
-        vector<vector<vector<int>>>
-            memo(m + 1, vector<vector<int>>(n + 1, vector<int>(s3_len + 1, -1)));
+        vector<vector<int>> memo(m + 1, vector<int>(n + 1, -1));
 
-        return solve(s1, s2, s3, 0, 0, 0, memo);
+        return solve(s1, s2, s3, 0, 0, memo);
     }
 };
