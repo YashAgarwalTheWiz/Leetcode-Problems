c0fee26fe63eecd50594a37d25236cdeef6e5da8
YashAgarwalTheWiz <143926886+YashAgarwalTheWiz@users.noreply.github.com>
2023-10-26 15:16:52 +0530

    Time: 12 ms (71.19%), Space: 38.6 MB (24.19%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
-    int solve(int index, int sum, int target, vector<int>& nums) {
+    int solve(int index, int sum, int target, vector<int>& nums, vector<vector<int>>& memo) {
         if (index == nums.size()) {
             return (sum == target) ? 1 : 0;
         }
+        if (memo[index][sum] != -1) {
+            return memo[index][sum];
+        }
 
-        int add = solve(index + 1, sum + nums[index], target, nums);
-        int sub = solve(index + 1, sum - nums[index], target, nums);
-        return add + sub;
+        int add = solve(index + 1, sum + nums[index], target, nums, memo);
+        int sub = solve(index + 1, sum - nums[index], target, nums, memo);
+        
+        return memo[index][sum] = add + sub;
     }
 
     int findTargetSumWays(vector<int>& nums, int target) {
-        return solve(0, 0, target, nums);
+        vector<vector<int>> memo(nums.size(), vector<int>(2001, -1));
+        return solve(0, 1000, target + 1000, nums, memo); 
     }
 };
