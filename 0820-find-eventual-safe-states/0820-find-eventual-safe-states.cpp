fa786b1c76eaa8cd778578eebaa59271744a32f9
YashAgarwalTheWiz <143926886+YashAgarwalTheWiz@users.noreply.github.com>
2023-11-07 22:25:39 +0530

    Time: 150 ms (56.88%), Space: 61.9 MB (24.04%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -1,44 +1,31 @@
 class Solution {
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int n = graph.size();
-        vector<int> result;
-        vector<int> color(n, 0); // 0: unvisited, 1: visiting, 2: visited and safe
-        vector<int> memo(n, -1); // Memoization array
-
-        for (int i = 0; i < n; i++) {
-            if (dfs(i, graph, color, memo)) {
-                result.push_back(i);
+        vector<vector<int>>newg(graph.size());
+        vector<int>indeg(graph.size(),0);
+        for(int i=0;i<graph.size();i++){
+            for(auto j:graph[i]){
+                newg[j].push_back(i);
+                indeg[i]++;
             }
         }
-
-        return result;
-    }
-
-    bool dfs(int i, vector<vector<int>>& graph, vector<int>& color, vector<int>& memo) {
-        if (color[i] == 2) {
-            return true; // This node is already visited and safe.
+        queue<int>q;
+        for(int i=0;i<indeg.size();i++){
+            if(indeg[i]==0)
+              q.push(i);
         }
-
-        if (color[i] == 1) {
-            return false; // This node is currently being visited, indicating a cycle.
-        }
-
-        if (memo[i] != -1) {
-            return memo[i] == 1; // Return cached result.
-        }
-
-        color[i] = 1; // Mark as visiting
-
-        for (int neighbor : graph[i]) {
-            if (!dfs(neighbor, graph, color, memo)) {
-                memo[i] = 0; // Mark as not safe
-                return false;
+        vector<int>ans;
+        while(!q.empty()){
+            int front=q.front();
+            q.pop();
+            ans.push_back(front);
+            for(auto i:newg[front]){
+                if(--indeg[i]==0)
+                  q.push(i);
             }
         }
+        sort(ans.begin(),ans.end());
+        return ans;
 
-        color[i] = 2; // Mark as visited and safe
-        memo[i] = 1; // Mark as safe
-        return true;
     }
 };
